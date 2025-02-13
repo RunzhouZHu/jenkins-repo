ee4a297c4f58cc2d930aed3c4ab5c382e359a988
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-13 13:53:25 +0200

    123

--- week5-inclass-assignment/src/test/java/runzhou/CalTest.java
+++ week5-inclass-assignment/src/test/java/runzhou/CalTest.java
@@ -8,18 +8,25 @@ class CalTest {
 
     @Test
     void sum() {
-        assertEquals(2, 2);
+        Cal cal = new Cal();
+        assertEquals(2, cal.sum(1, 1));
     }
 
     @Test
     void sub() {
+        Cal cal = new Cal();
+        assertEquals(0, cal.sub(1, 1));
     }
 
     @Test
     void mul() {
+        Cal cal = new Cal();
+        assertEquals(1, cal.mul(1, 1));
     }
 
     @Test
     void div() {
+        Cal cal = new Cal();
+        assertEquals(1, cal.div(1, 1));
     }
 }
