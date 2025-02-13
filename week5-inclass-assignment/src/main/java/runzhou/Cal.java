63e68153521c2f3e281f71236f0b7796eb1683b2
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-13 13:49:06 +0200

    123

---
+++ week5-inclass-assignment/src/main/java/runzhou/Cal.java
@@ -0,0 +1,16 @@
+package runzhou;
+
+public class Cal {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public int sub(int a, int b) {
+        return a - b ;
+    }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    public int div(int a, int b) {
+        return a / b;
+    }
+}
---
+++ week5-inclass-assignment/src/test/java/runzhou/CalTest.java
@@ -0,0 +1,25 @@
+package runzhou;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalTest {
+
+    @Test
+    void sum() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    void sub() {
+    }
+
+    @Test
+    void mul() {
+    }
+
+    @Test
+    void div() {
+    }
+}
