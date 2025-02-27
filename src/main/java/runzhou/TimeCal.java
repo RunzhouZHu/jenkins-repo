baf0987961149838fc3883f17c997815aa3b4af8
Runzhou Zhu <runzhouz@metropolia.fi>
2025-02-27 13:31:33 +0200

    123

---
+++ src/main/java/runzhou/TimeCal.java
@@ -0,0 +1,12 @@
+package runzhou;
+
+public class TimeCal {
+
+    public static void main(String[] args) {
+        System.out.println("Time needs " + timeCalculator(100.0, 100.0) + " seconds");
+    }
+
+    public static double timeCalculator(double x, double v) {
+        return x / v;
+    }
+}
---
+++ src/test/java/runzhou/TimeCalTest.java
@@ -0,0 +1,12 @@
+package runzhou;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TimeCalTest {
+
+    @Test
+    void timeCalculator() {
+        assertEquals(1.0, TimeCal.timeCalculator(100.0, 100.0), 0);
+    }
+}
